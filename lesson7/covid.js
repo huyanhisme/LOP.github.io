975ba9abe641ad4cc7cb1520adf493a46d641c66
huyanhisme <letranhuyanh1466@gmail.com>
2021-10-02 17:40:59 +0700

    btvn day anh oi

--- lesson7/covid.js
+++ lesson7/covid.js
@@ -1,56 +1,77 @@
-let getData = async () => {
+let getData = async (city) => {
     let allData = await fetch("https://static.pipezero.com/covid/data.json?fbclid=IwAR0ZAK4Kw7s9G9kM0ptiX3lOEBnpA2ZY9gBDhGf4HEC8fE8cFfKu4NTEkSk")
 
     let data = await allData.json()
-    
-    console.log(data);
 
-    renderData(data)
+
+
+    renderData(data, city)
 }
 
-let renderData = (data) => {
-
-    // let worldCase = data.total.world.cases
-    // let worldDeath = data.total.world.death
-    // let worldRecovered = data.total.world.recovered
-
-    // let internalCase = data.total.internal.cases
-    // let internalDeath = data.total.internal.death
-    // let internalRecovered = data.total.internal.recovered
+let renderData = (data, city) => {
 
     let locations = data.locations
-    let cases = data.locations.cases
-    let death = data.locations.death
-    let recovered = data.locations.recovered
-    
-    var arr = [ ['name', locations ]  ];
-    console.log(arr);
 
-    var obj = Object.fromEntries(arr);
-    console.log(obj);
+    var arr = [].concat(locations)
+
+    arr.filter((searchInput) => {
+        if (searchInput.name === city) {
+            console.log(searchInput)
+            let name = searchInput.name
+            let cases = searchInput.cases
+            let death = searchInput.death
+            let recovered = searchInput.recovered
+
+            let domCovid = document.querySelector('.theme')
+
+            let html = `
+            <h2>${name}</h2>
+            <h2>Cases : ${cases} </h2>
+            <h2>Dead : ${death}</h2>
+            <h2>Recovered : ${recovered}</h2>
+            <img src="" alt="">
+            <p></p>`
+
+            domCovid.innerHTML = html
+        }
+    });
 
 
+    if (city === 'internal') {
+        let internalCases = data.total.internal.cases
+        let internalDeath = data.total.internal.death
+        let internalRecovered = data.total.internal.recovered
 
-    // let arrayName = [data]
+        let domCovid = document.querySelector('.theme')
 
-    // arrayName.filter((inputSearch) => {
-    //     return inputSearch == inputSearch.name
-    // })
+        let html = `
+        <h2>Internal</h2>
+        <h2>Cases : ${internalCases} </h2>
+        <h2>Dead : ${internalDeath}</h2>
+        <h2>Recovered : ${internalRecovered}</h2>
+        <img src="" alt="">
+        <p></p>`
 
-    
+        domCovid.innerHTML = html
 
+    } else if (city === 'world') {
+        let worldCases = data.total.world.cases
+        let worldDeath = data.total.world.death
+        let worldRecovered = data.total.world.recovered
 
-    let domCovid = document.querySelector('.theme')
+        let domCovid = document.querySelector('.theme')
 
-    let html = `
-    <h2>${cases}</h2>
-    <h2>Cases : ${death} </h2>
-    <h2>Dead : ${death}</h2>
-    <h2>Recovered : ${recovered}</h2>
-    <img src="" alt="">
-    <p></p>`
+        let html = `
+            <h2>World</h2>
+            <h2>Cases : ${worldCases} </h2>
+            <h2>Dead : ${worldDeath}</h2>
+            <h2>Recovered : ${worldRecovered}</h2>
+            <img src="" alt="">
+            <p></p>`
 
-    domCovid.innerHTML = html
+        domCovid.innerHTML = html
+    }
+
 }
 
 
@@ -61,9 +82,8 @@ Search.onsubmit = (e) => {
 
     let city = Search.city.value;
 
+
     getData(city)
 }
-getData()
-
 
 
